215dd1f8182da10452e233bba5d0cb4dbd2aad12
lightbits <simenh.jobb@gmail.com>
2016-03-22 16:02:11 +0100

    Roomba interaction

--- game.cpp
+++ game.cpp
@@ -1,3 +1,9 @@
+/* TODO:
+* Roomba interaction
+* Graphics
+* Highscore display
+*/
+
 #include "platform.h"
 #include <cstdio>
 #define IFKEYDOWN(KEY) if (input.key.down[SDL_SCANCODE_##KEY])
@@ -66,13 +72,43 @@ struct Pendulum
     vec2 position;
     vec2 Dposition;
     r32 mass;
+    r32 radius;
 } pendulum;
 
 struct Roomba
 {
-    vec2 position;
-    vec2 Dposition;
+    r32 x;
+    r32 y0;
+    r32 y1;
+    r32 y2;
+    r32 radius;
+    r32 direction;
+    r32 Rdirection;
+    r32 turn_timer;
+    r32 turn_timer0;
+    r32 activate_timer;
+    r32 activate_timer0;
+    r32 speed;
 } roomba;
+
+struct Animations
+{
+    struct MagnetTimer
+    {
+        r32 prev_progress;
+        r32 progress;
+    } magnet_timer;
+
+    struct MagnetBroken
+    {
+        r32 progress;
+    };
+
+    struct MagnetSuccess
+    {
+        r32 progress;
+    };
+} anims;
 
 struct World
 {
@@ -105,8 +141,8 @@ void game_init()
     }
     {
         world.floor_level = 0.0f;
-        world.green_line = 1.0f;
-        world.red_line = -1.0f;
+        world.green_line = 2.0f;
+        world.red_line = -2.0f;
         world.g = 9.81f;
         world.right = +2.0f;
         world.left = -2.0f;
@@ -119,6 +155,7 @@ void game_init()
         spring.d = 1.0f;
 
         pendulum.mass = 0.05f;
+        pendulum.radius = 0.1f;
 
         player.mass = 1.0f;
         player.arm = 0.5f;
@@ -127,18 +164,30 @@ void game_init()
         player.motor_constant = 0.8f*(player.mass+pendulum.mass)*world.g;
         player.l_motor = compute_hover_voltage();
         player.r_motor = player.l_motor;
+
+        roomba.radius = 0.5f;
+        roomba.turn_timer0 = 10.0f;
+        roomba.speed = 0.33f;
+        roomba.Rdirection = -1.0f;
+        roomba.y0 = world.floor_level+0.1f;
+        roomba.y1 = world.floor_level+0.3f;
+        roomba.y2 = world.floor_level+0.6f;
+
+        roomba.activate_timer0 = 1.0f;
     }
     {
         player.theta = 0.0f;
         player.Dtheta = 0.0f;
-        player.position = m_vec2(0.0f, 1.0f);
+        player.position = m_vec2(0.0f, 1.5f);
         player.Dposition = m_vec2(0.0f, 0.0f);
 
         pendulum.position = m_vec2(player.position.x, player.position.y-spring.l0);
         pendulum.Dposition = m_vec2(0.0f, 0.0f);
 
-        roomba.position = m_vec2(0.0f, world.floor_level);
-        roomba.Dposition = m_vec2(0.0f, 0.0f);
+        roomba.x = 0.0f;
+        roomba.direction = -1.0f;
+        roomba.turn_timer = roomba.turn_timer0;
+        roomba.activate_timer = roomba.activate_timer0;
     }
     #if 0
     {
@@ -198,6 +247,10 @@ void pxCircle(vec2 center, r32 radius, int n)
 
 void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
 {
+    {
+        anims.magnet_timer.prev_progress = anims.magnet_timer.progress;
+    }
+
     // update
     {
         // key input
@@ -232,7 +285,6 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
             if (player.r_motor > 1.0f) player.r_motor = 1.0f;
             if (player.r_motor < 0.0f) player.r_motor = 0.0f;
         }
-        // end key input
 
         // spring force
         r32 spring_f = 0.0f;
@@ -301,7 +353,6 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
                 pendulum.Dposition = m_vec2(0.0f, 0.0f);
             }
         }
-        // end update player
 
         // update pendulum
         {
@@ -309,9 +360,20 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
             vec2 s_force = spring_f*v_pendulum_to_player;
             vec2 g_force = m_vec2(0.0f, -pendulum.mass*world.g);
             vec2 n_force = m_vec2(0.0f, 0.0f);
-            if (pendulum.position.y < world.floor_level)
+
+            // contact forces
             {
-                n_force.y = 50.0f*(world.floor_level-pendulum.position.y);
+                r32 ay = pendulum.position.y-pendulum.radius;
+                r32 by = world.floor_level;
+                r32 cy = roomba.y1;
+                if (ay < by)
+                {
+                    n_force.y = 50.0f*(by-ay);
+                }
+                if (ay < cy && m_abs(pendulum.position.x-roomba.x) < roomba.radius)
+                {
+                    n_force.y = 50.0f*(cy-ay);
+                }
             }
             vec2 delta_v = pendulum.Dposition - player.Dposition;
             vec2 f_force = -0.1f*delta_v*m_length(delta_v);
@@ -321,7 +383,6 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
             pendulum.Dposition += DDposition * dt;
             pendulum.position += pendulum.Dposition * dt;
         }
-        // end update pendulum
 
         // update camera
         {
@@ -337,13 +398,67 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
 
             Dposition += DDposition*delta_time;
             position += Dposition*delta_time;
-            r32 radius = 2.0f;
+            // static r32 radius = 2.0f;
+            // static r32 Dradius = 0.0f;
+            // r32 Rradius = 2.0f;
+            // {
+            //     r32 distance = m_length(pendulum.position-m_vec2(roomba.x, roomba.y1));
+            //     r32 distance0 = 0.05f;
+            //     r32 distance1 = 2.0f;
+            //     Rradius = 1.8f + (2.2f-1.8f)*(distance-distance0)/(distance1-distance0);
+            // }
+
+            // {
+            //     r32 DDradius = 10.0f*(Rradius-radius)-2.0f*Dradius;
+            //     Dradius += DDradius*delta_time;
+            //     radius += Dradius*delta_time;
+            // }
+            r32 radius = 3.0f;
+
             world.right = (mode.width / (r32)mode.height)*(position.x+radius);
             world.left = (mode.width / (r32)mode.height)*(position.x-radius);
             world.top = position.y+radius;
             world.bottom = position.y-radius;
         }
-        // end update camera
+
+        // update roomba
+        {
+            roomba.x += roomba.direction*roomba.speed*delta_time;
+            roomba.turn_timer -= delta_time;
+            if (roomba.turn_timer < 0.0f)
+            {
+                roomba.turn_timer = roomba.turn_timer0;
+                roomba.Rdirection *= -1.0f;
+            }
+            roomba.direction += 5.0f*(roomba.Rdirection-roomba.direction)*delta_time;
+
+            bool magnet_activated = false;
+            if (m_abs(pendulum.position.x-roomba.x) < roomba.radius &&
+                pendulum.position.y > roomba.y1 &&
+                pendulum.position.y < roomba.y2)
+            {
+                magnet_activated = true;
+            }
+
+            if (magnet_activated)
+            {
+                roomba.activate_timer -= delta_time;
+                anims.magnet_timer.progress = (roomba.activate_timer0-roomba.activate_timer)/roomba.activate_timer0;
+            }
+            else
+            {
+                anims.magnet_timer.progress = -1.0f;
+                roomba.activate_timer = roomba.activate_timer0;
+            }
+
+            if (roomba.activate_timer < 0.0f)
+            {
+                // todo: play celebratory animation
+                // START_ANIM(magnet_success);
+                roomba.Rdirection *= -1.0f;
+                roomba.activate_timer = roomba.activate_timer0;
+            }
+        }
     }
     // end update
 
@@ -431,7 +546,110 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
             glEnd();
 
             pxColor(XRGB(0x1A1A1AFF));
-            pxCircle(pendulum.position, 0.1f, 32);
+            pxCircle(pendulum.position, pendulum.radius, 32);
+        }
+
+        // draw roomba
+        {
+            glBegin(GL_TRIANGLES);
+            {
+                r32 x0 = roomba.x-roomba.radius;
+                r32 x1 = roomba.x+roomba.radius;
+                r32 y0 = roomba.y0;
+                r32 y1 = roomba.y1;
+                glColor4f(XRGB(0x1A1A1AFF));
+                glVertex2f(x0, y0);
+                glVertex2f(x1, y0);
+                glVertex2f(x1, y1);
+                glVertex2f(x1, y1);
+                glVertex2f(x0, y1);
+                glVertex2f(x0, y0);
+            }
+            {
+                r32 x0 = roomba.x-0.8f*roomba.radius;
+                r32 x1 = roomba.x+0.8f*roomba.radius;
+                r32 y0 = roomba.y1;
+                r32 y1 = roomba.y2;
+                glColor4f(XRGB(0xE03C2877)); glVertex2f(x0, y0);
+                glColor4f(XRGB(0xE03C2877)); glVertex2f(x1, y0);
+                glColor4f(XRGB(0xE03C2822)); glVertex2f(x1, y1);
+                glColor4f(XRGB(0xE03C2822)); glVertex2f(x1, y1);
+                glColor4f(XRGB(0xE03C2822)); glVertex2f(x0, y1);
+                glColor4f(XRGB(0xE03C2877)); glVertex2f(x0, y0);
+            }
+            glEnd();
+
+            r32 inner_eye = 0.1f;
+            r32 outer_eye = 0.7f;
+            r32 eye_radius = 0.2f;
+
+            glBegin(GL_LINES);
+            glColor4f(XRGB(0xE2D7B5FF));
+            {
+                // left eye
+                r32 dx = -outer_eye+(-inner_eye+outer_eye)*(0.5f+0.5f*roomba.direction);
+                r32 cx = roomba.x+roomba.radius*dx;
+                r32 x0 = cx-eye_radius*roomba.radius;
+                r32 x1 = cx+eye_radius*roomba.radius;
+                glVertex2f(x0, world.floor_level+0.2f);
+                glVertex2f(x1, world.floor_level+0.2f);
+            }
+            {
+                // right eye
+                r32 dx = inner_eye+(outer_eye-inner_eye)*(0.5f+0.5f*roomba.direction);
+                r32 cx = roomba.x+roomba.radius*dx;
+                r32 x0 = cx-eye_radius*roomba.radius;
+                r32 x1 = cx+eye_radius*roomba.radius;
+                glVertex2f(x0, world.floor_level+0.2f);
+                glVertex2f(x1, world.floor_level+0.2f);
+            }
+            glEnd();
+        }
+
+        // draw magnet timer
+        if (anims.magnet_timer.progress > -1.0f)
+        {
+            glBegin(GL_TRIANGLES);
+            glColor4f(XRGB(0xE03C28FF));
+            r32 t = anims.magnet_timer.progress*TWO_PI;
+            r32 radius = 0.3f;
+            vec2 center = m_vec2(roomba.x, roomba.y1+0.5f);
+            int n = 64;
+            for (int i = 0; i < n; i++)
+            {
+                r32 t0 = TWO_PI*i/(r32)n;
+                r32 t1 = TWO_PI*(i+1)/(r32)n;
+                if (t1 > t)
+                    break;
+                r32 c0 = radius*cos(t0);
+                r32 s0 = radius*sin(t0);
+                r32 c1 = radius*cos(t1);
+                r32 s1 = radius*sin(t1);
+                glVertex2f(center.x, center.y);
+                glVertex2f(center.x+c0, center.y+s0);
+                glVertex2f(center.x+c1, center.y+s1);
+            }
+            glEnd();
+        }
+
+        if (anims.magnet_timer.progress < 0.0f &&
+            anims.magnet_timer.prev_progress > 1.0f)
+        {
+            // PLAY_ANIM(2.0f)
+            // {
+
+            // }
+            printf("success\n");
+        }
+        if (anims.magnet_timer.progress < 0.0f &&
+            anims.magnet_timer.prev_progress > 0.0f &&
+            anims.magnet_timer.prev_progress < 1.0f)
+        {
+            printf("broken\n");
+            // PLAY_ANIM(2.0f)
+            // {
+
+            // }
         }
     }
 
@@ -441,26 +659,28 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
         glLoadIdentity();
 
         #if 0
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
         {
-            r32 Ax = +2.0f / mode.width;
-            r32 Bx = -1.0f;
-            r32 Ay = -2.0f / mode.height;
-            r32 By = +1.0f;
-            r32 data[] = {
-                Ax,   0.0f, 0.0f, 0.0f,
-                0.0f, Ay,   0.0f, 0.0f,
-                0.0f, 0.0f, 1.0f, 0.0f,
-                Bx,   By,   0.0f, 1.0f
-            };
-            glLoadMatrixf(data);
-        }
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            {
+                r32 Ax = +2.0f / mode.width;
+                r32 Bx = -1.0f;
+                r32 Ay = -2.0f / mode.height;
+                r32 By = +1.0f;
+                r32 data[] = {
+                    Ax,   0.0f, 0.0f, 0.0f,
+                    0.0f, Ay,   0.0f, 0.0f,
+                    0.0f, 0.0f, 1.0f, 0.0f,
+                    Bx,   By,   0.0f, 1.0f
+                };
+                glLoadMatrixf(data);
+            }
 
-        glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-        pxPrint(100.0f, 200.0f, "You!");
-        glDisable(GL_BLEND);
+            glEnable(GL_BLEND);
+            glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+            pxPrint(100.0f, 200.0f, "You!");
+            glDisable(GL_BLEND);
+        }
         #endif
 
         {
@@ -471,6 +691,9 @@ void game_tick(Input input, VideoMode mode, r32 elapsed_time, r32 delta_time)
             {
                 game_init();
             }
+
+            SliderFloat("Roomba direction", &roomba.Rdirection, -1.0f, +1.0f);
+            SliderFloat("Roomba timer", &roomba.activate_timer, 0.0f, roomba.activate_timer0);
         }
     }
     // end render
